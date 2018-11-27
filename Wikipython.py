a1a387109fc058afd8da5a42f3baa30d939f6227
Sumit Mukhija <sumit@jova.io>
2018-11-27 14:02:21 +0530

    Basic version complete

--- Wikipython.py
+++ Wikipython.py
@@ -1,17 +1,35 @@
-import urllib2
+#!/usr/bin/env python3
+import requests
 
 class Wikipython:
 
 	baseUrl = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles="
 
 	def __init__(self, searchQuery):
-		processedUrl = baseUrl+searchQuery
-		content = urllib2.urlopen(processedUrl).read()
-		print(content)
+		processedUrl = self.baseUrl+searchQuery
+		response = requests.get(processedUrl)
+		json = response.json()
+		requiredSubJson = json['query']['pages']
+		keyList = list(requiredSubJson.keys())
+		pageId = keyList[0]
+		try:
+			if pageId:
+				info = requiredSubJson[pageId]['extract']
+				listOfStatements = info.split('.')
+				firstFiveStatements = listOfStatements[:6]
+				print(' '.join(firstFiveStatements))
+			else:
+				print("\nCould not find anything on ", searchQuery,"\n")
+		except KeyError:
+			print("\nCould not find anything on ", searchQuery, " at this moment.\n")
 
 
 
-Wikipython('Software')
+
+
+search = input('Search for: ')
+Wikipython(search)
+
 
 
 
